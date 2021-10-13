a6d3e707b36369c378fc75abee9f5d5effad1ef6
domi555 <66876360+domi555@users.noreply.github.com>
2021-10-13 10:34:49 +0200

    Update

---
+++ client/src/components/CarCards.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-main>
+    <div class="d-flex flex-wrap justify-center mt-4">
+      <CarCard
+        v-for="(car, i) in cars"
+        :key="i"
+        :car="car"
+        class="ma-3"
+        @reload="$emit('reload')"
+      ></CarCard>
+    </div>
+  </v-main>
+</template>
+
+<script>
+import CarCard from './CarCard.vue';
+
+export default {
+  components: {
+    CarCard,
+  },
+  props: {
+    cars: {
+      type: Array,
+    },
+  },
+};
+</script>
